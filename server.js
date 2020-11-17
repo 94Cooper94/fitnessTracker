1f8e162a0af468532de5ed2f66ed3c0e9040cb4a
Cooper <eliteswimmer42@gmail.com>
2020-11-17 11:37:01 -0600

    updating server.js with pathing

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/notes", (req, res) => {
-  db.Note.find({})
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
     .then(dbNote => {
       res.json(dbNote);
     })
@@ -27,26 +27,33 @@ app.get("/notes", (req, res) => {
     });
 });
 
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// app.get("/user", (req, res) => {
+//   db.User.find({})
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// app.post("/submit", ({ body }, res) => {
+//   db.Note.create(body)
+//     .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// put call for updating workout (pushing in new data)
+
+// two more app.get calls for html routes
+
+// res.sendFile stats
+// res.sendFile exercise
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
