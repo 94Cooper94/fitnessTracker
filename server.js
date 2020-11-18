0790e52f57716495ac5d37a63ef50c6e49fbcc33
Cooper <eliteswimmer42@gmail.com>
2020-11-18 15:44:28 -0600

    setting up mongoDB and deploying to heroku

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitnessTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
